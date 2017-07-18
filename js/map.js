8aef58b420fab7beac55686967859df8c70dcaf9
Bert Spaan <bert.spaan@gmail.com>
2017-07-18 18:05:41 -0400

    load old submissions

--- js/map.js
+++ js/map.js
@@ -16,6 +16,56 @@ var geojson = {
   features: []
 }
 
+function toFeatures (data) {
+  if (data.submission.step === 'location') {
+    return [{
+      type: 'Feature',
+      properties: {
+        step: data.submission.step,
+        uuid: data.item.id
+      },
+      geometry: data.submission.data.geometry
+    }]
+  } else if (data.submission.step === 'bearing') {
+    var point = data.submission.data.geometry.geometries[0].coordinates
+    var lineString = data.submission.data.geometry.geometries[1].coordinates
+
+    return [
+      {
+        type: 'Feature',
+        properties: {
+          step: data.submission.step,
+          uuid: data.item.id
+        },
+        geometry: {
+          type: 'LineString',
+          coordinates: [
+            lineString[0],
+            point,
+            lineString[1],
+          ]
+        }
+      },
+      {
+        type: 'Feature',
+        properties: {
+          step: data.submission.step,
+          uuid: data.item.id
+        },
+        geometry: {
+          type: 'Polygon',
+          coordinates: [[
+            point,
+            lineString[0],
+            lineString[1],
+            point
+          ]]
+        }
+      }
+    ]
+  }
+}
+
 map.on('load', function () {
   map.addControl(new mapboxgl.NavigationControl())
 
@@ -72,70 +122,43 @@ map.on('load', function () {
     ]
   })
 
+  getSubmissions()
+
   socket.on('submission', function (data) {
     if (!data || !data.submission || data.task.id !== config.taskId || data.submission.skipped) {
       return
     }
 
-    var features
-
-    if (data.submission.step === 'location') {
-      features = [{
-        type: 'Feature',
-        properties: {
-          step: data.submission.step,
-          uuid: data.item.id
-        },
-        geometry: data.submission.data.geometry
-      }]
-
-      console.log(features)
-    } else if (data.submission.step === 'bearing') {
-      var point = data.submission.data.geometry.geometries[0].coordinates
-      var lineString = data.submission.data.geometry.geometries[1].coordinates
-
-      features = [
-        {
-          type: 'Feature',
-          properties: {
-            step: data.submission.step,
-            uuid: data.item.id
-          },
-          geometry: {
-            type: 'LineString',
-            coordinates: [
-              lineString[0],
-              point,
-              lineString[1],
-            ]
-          }
-        },
-        {
-          type: 'Feature',
-          properties: {
-            step: data.submission.step,
-            uuid: data.item.id
-          },
-          geometry: {
-            type: 'Polygon',
-            coordinates: [[
-              point,
-              lineString[0],
-              lineString[1],
-              point
-            ]]
-          }
-        }
-      ]
-
-    }
-
+    var features = toFeatures(data)
     geojson.features = geojson.features.concat(features)
-
     map.getSource('submissions').setData(geojson)
   })
 })
 
-// d3.json(config.host + 'tasks/' + config.taskId + '/submissions/all', function (json) {
-//   console.log(json)
-// })
+function getSubmissions () {
+  d3.json(config.host + 'tasks/' + config.taskId + '/submissions/all', function (json) {
+    var submissions = json.map(function (item) {
+      return item.submissions
+        .map(function (submission) {
+          return submission.steps.map(function (step) {
+            return {
+              submission: step,
+              organization: {
+                id: item.organization.id
+              },
+              item: {
+                id: item.item.id
+              },
+              task: {
+                id: item.task.id
+              }
+            }
+          })
+        })
+    })
+
+    var features = R.flatten(submissions).map(toFeatures)
+    geojson.features = R.flatten(features)
+    map.getSource('submissions').setData(geojson)
+  })
+}
